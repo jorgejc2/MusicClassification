ddb6f3e60895f33c63461a1e14f7b8b9840699e6
jorgejc2 <jorgejc2@illinois.edu>
2023-01-18 12:29:39 -0600

    dsp may start working as a python module but at the expense of using stft example code

--- test_module.py
+++ test_module.py
@@ -1,8 +1,8 @@
 # import sys
 # sys.path.append('./build/src')
 
-# from build.src.dsp_module import *
-import dsp_module
+from build.src.dsp_module import *
+# import dsp_module
 
 print(test_func())
-print(py_thread_per_block())
+print(get_thread_per_block())
