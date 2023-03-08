9f0b479b4a6b6af8c240fdc06170dd138ea5ca8c
jorgejc2 <jorgejc2@illinois.edu>
2023-03-08 15:42:33 -0600

    basic neural network approach leads to extremely low accuracies, converting to knn and optimizing cuda further

--- pybind_modules/dsp.pybind.cu
+++ pybind_modules/dsp.pybind.cu
@@ -285,7 +285,7 @@ PYBIND11_MODULE(dsp_module, module_handle) {
     module_handle.def("cuSTFT", [](vector<float> samples, int sample_rate, int NFFT, int noverlap, bool one_sided, int window, bool mag) {
         py::array out = py::cast(pybind_cuSTFT(samples, sample_rate, NFFT, noverlap, one_sided, window, mag));
         return out;
-    }, py::arg("samples"), py::arg("sample_rate"), py::arg("NFFT"), py::arg("noverlap"), py::arg("one_sided"), py::arg("window"), py::arg("mag"), py::return_value_policy::move);
+    }, py::arg("samples"), py::arg("sample_rate"), py::arg_v("NFFT", 1024, "int"), py::arg_v("noverlap", -1, "int"), py::arg_v("one_sided", true, "bool"), py::arg_v("window", 2,"int"), py::arg_v("mag", true, "bool"), py::return_value_policy::move);
     module_handle.def("test_cuda", &test_cuda);
 /* commented out but kept for reference for adding a class */
 
